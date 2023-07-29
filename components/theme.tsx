5ba758edafa264f6400bd0ca803881e21b0655c7
Jerome Wong <jeromewongjh@gmail.com>
2023-07-29 19:39:58 +0800

    fix: hydration warning from next-theme

--- components/theme.tsx
+++ components/theme.tsx
@@ -1,20 +1,18 @@
+"use client";
+
 import { useTheme } from "next-themes";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Moon, Sun } from "lucide-react";
 
 const Theme = () => {
     const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
 
-    const changeTheme = (theme: string) => {
-        if (theme === "light") {
-            setTheme("light");
-        } else if (theme === "dark") {
-            setTheme("dark");
-        } else {
-            setTheme("system");
-        }
-    };
+    useEffect(() => setMounted(true), []);
 
+    if (!mounted) {
+        return null;
+    }
     return (
         <div
             className=" rounded-md bg-white  p-2 transition duration-200 hover:bg-secondary/20 dark:bg-black dark:hover:bg-secondary/30"
