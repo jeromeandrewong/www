24b8b918e6ad1b27f2d3eaceb29bd80974e76d34
Jerome Wong <jeromewongjh@gmail.com>
2023-07-31 19:57:33 +0800

    style: theme icon hover state

--- components/theme.tsx
+++ components/theme.tsx
@@ -15,7 +15,7 @@ const Theme = () => {
     }
     return (
         <div
-            className=" rounded-md bg-white  p-2 transition duration-200 hover:bg-secondary/20 dark:bg-black dark:hover:bg-secondary/30"
+            className="rounded-md bg-white p-2  transition duration-200 hover:cursor-pointer hover:bg-secondary/20 dark:bg-black dark:hover:bg-secondary/30"
             onClick={() => {
                 theme === "light" ? setTheme("dark") : setTheme("light");
             }}
